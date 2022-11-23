5e3fa79d2db1d7b738ab8238170f7a7d52fd3bbe
majoduke <77840734+majoduke@users.noreply.github.com>
2022-11-23 23:15:19 +0530

    Delete q10(port 10000).js

--- q10(port 10000).js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-http.createServer((request,response)=>{
-	response.write("Hello World, This is my Node.js server");
-	response.end();
-})
-.listen(10000,()=>console.log('server started at port 10000'));
